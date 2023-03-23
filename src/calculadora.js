c8684442ae1989151e43aca2e98e79501c28650a
Adrian Montano <adrian.montano2011@yahoo.com>
2023-03-23 09:24:50 -0400

    Calcula la suma para una cadena de dos digitos separados por coma

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,8 +1,18 @@
 function calculadora(cadena) {
   if(cadena=="")
     return 0;
-  else
+  else if(cadena.length == 1)
+  {
     return parseInt(cadena);
+  }
+  else
+  {
+    let suma=0;
+    let numeros = cadena.split(","); //divide en un array de aquellos separados por coma
+    suma = suma + parseInt(numeros[0]);
+    suma = suma + parseInt(numeros[1]);
+    return suma;
+  }
 }
 
 export default calculadora;
--- src/calculadora.spec.js
+++ src/calculadora.spec.js
@@ -7,4 +7,7 @@ describe("Calcular la suma de una cadena", () => {
   it("Calcular con una cadena de un numero", () => {
     expect(calculadora("1")).toEqual(1);
   });
+  it("Calcular con una cadena de dos numeros separados por coma", () => {
+    expect(calculadora("1,2")).toEqual(3);
+  });
 });
