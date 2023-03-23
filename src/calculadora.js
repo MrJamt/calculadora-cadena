11bfbb92ccc5b38e8f8ed6482e5d4d19f5d1dedd
Adrian Montano <adrian.montano2011@yahoo.com>
2023-03-23 08:52:05 -0400

    Calcula la suma para una cadena de un digito

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,5 +1,8 @@
 function calculadora(cadena) {
-  return 0;
+  if(cadena=="")
+    return 0;
+  else
+    return parseInt(cadena);
 }
 
 export default calculadora;
--- src/calculadora.spec.js
+++ src/calculadora.spec.js
@@ -4,4 +4,7 @@ describe("Calcular la suma de una cadena", () => {
   it("Vacia", () => {
     expect(calculadora("")).toEqual(0);
   });
+  it("Calcular con una cadena de un numero", () => {
+    expect(calculadora("1")).toEqual(1);
+  });
 });
