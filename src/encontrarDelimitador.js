b28d08874c21059f8f4266f7213014d107fafc61
Adrian Montano <adrian.montano2011@yahoo.com>
2023-03-30 09:48:34 -0400

    Se refactorizo la anidacion de ifs en la funcion encontrarDelimitador para tener una mejor abstraccion

--- src/encontrarDelimitador.js
+++ src/encontrarDelimitador.js
@@ -3,22 +3,8 @@ function encontrarDelimitador(cadenaNumeros) {
   let delimitador="";
   if(cadenaNumeros[0]==cadenaNumeros[1] && cadenaNumeros[0]=="/") //empieza el/los delimitador/es
   {
-        let posicion_caracter=3;
-        let existeDelimitador = true;
-        while(existeDelimitador)
-        {
-            delimitador+=cadenaNumeros[posicion_caracter];
-            posicion_caracter++;
-            if(cadenaNumeros[posicion_caracter]=="]")
-            {
-              delimitadores.push(delimitador);
-              delimitador="";     
-              if(cadenaNumeros[posicion_caracter+1]=="[")
-                posicion_caracter+=2;
-              else
-                existeDelimitador=false;
-            }
-        }
+    delimitador = cadenaNumeros.slice(cadenaNumeros.indexOf("[")+1,cadenaNumeros.lastIndexOf("] "));
+    delimitadores = delimitador.split("][");
   }
   return delimitadores;
 }
